c336a71db6e46a5997deeeae21a57035e75e9020
Tribuster <trib_reg@mail.ru>
2022-04-21 13:07:19 +0600

    Added homework_001_03

---
+++ Homework_001_03.py
@@ -0,0 +1,6 @@
+#   3. Узнайте у пользователя число n.
+#   Найдите сумму чисел n + nn + nnn.
+#   Например, пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369.
+
+number_n = int(input("Enter a number from 0 to 9"))
+print(number_n + number_n * 11 + number_n * 111)
