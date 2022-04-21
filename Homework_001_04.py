54149a05a8c3f6a1eae5007ccea8d8a77199bb8c
Tribuster <trib_reg@mail.ru>
2022-04-21 14:18:56 +0600

    Added homework_001_04

---
+++ Homework_001_04.py
@@ -0,0 +1,16 @@
+#   4. Пользователь вводит целое положительное число.
+#   Найдите самую большую цифру в числе.
+#   Для решения используйте цикл while и арифметические операции.
+
+number_x = int(input("Enter a number from 0 to 9 "))
+max_x = number_x % 10
+
+while number_x > 1:
+    number_x = number_x // 10
+    if number_x % 10 > max_x:
+        max_x = number_x % 10
+    elif number_x > 9:
+        pass
+
+print("The maximum digit in a number:", max_x)
+
