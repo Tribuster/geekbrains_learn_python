9aa4586c30c65031ed19cb7edd2ab6049d0d1caf
Tribuster <trib_reg@mail.ru>
2022-04-21 12:22:40 +0600

    Added homework_001_01

---
+++ Homework_001_01.py
@@ -0,0 +1,11 @@
+#   1. Поработайте с переменными, создайте несколько, выведите на экран.
+#   Запросите у пользователя некоторые числа и строки и сохраните в переменные,
+#   затем выведите на экран.
+
+print('Hi, my dear!' )
+name_user = input('What is your name?.. ')
+print('Hello,', name_user + '!')
+year_of_birth_yser = int(input("In what year were you born? "))
+year_now = int(input("Аnd what year is it? "))
+age_user_now = year_now - year_of_birth_yser
+print('Oh', name_user, 'are you', age_user_now)
